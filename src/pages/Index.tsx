662adf365c65814396bfef76e7e8e42dbe9e6c9d
Haris Mughal <harismirza3456@gmail.com>
2025-05-12 05:26:49 +0500

    community section temp -r

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,7 +4,7 @@ import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import WhatWeOffer from '@/components/WhatWeOffer';
-import Community from '@/components/Community';
+// import Community from '@/components/Community';
 import Admins from '@/components/Admins';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
@@ -19,7 +19,7 @@ const Index: React.FC = () => {
         <Hero />
         <About />
         <WhatWeOffer />
-        <Community />
+        {/* <Community /> */}
         <Admins />
         <Contact />
         <Footer />
